e81b5ca86de78cf3a038e08958ffbb9cc2be7ac2
dennismanoila <162413304+dennismanoila@users.noreply.github.com>
2025-01-12 18:02:16 +0200

    DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/library_schema";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Hudavicase07_";
+    private static final String PASSWORD = "pass";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
